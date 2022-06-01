d4dd39ea28af1f2906d253fb17747bece4fc1a13
Pablo <pmella@buk.cl>
2022-06-01 15:42:27 -0400

    ruby: Creamos la versión 1 del post employees, con valores dummies

---
+++ post-employees.rb
@@ -0,0 +1,34 @@
+#Método POST de Empleados
+
+domain = 'ACÁ VA EL DOMINIO'
+token = 'ACÁ VA EL TOKEN'
+path = "https://#{domain}.buk.cl/api/v1/chile/employees"
+#declaramos las variables para invocar el endpoint
+resource = RestClient::Resource.new(path, headers: {auth_token: token})
+json_post = {
+  "first_name": "string",
+  "surname": "string",
+  "second_surname": "string",
+  "rut": "string",
+  "code_sheet": "string",
+  "nationality": "TJ",
+  "gender": "M",
+  "civil_status": "Casado",
+  "birthday": "2022-06-01",
+  "email": "string",
+  "personal_email": "string",
+  "location_id": 0,
+  "address": "string",
+  "city": "string",
+  "payment_method": "Transferencia Bancaria",
+  "bank": "Security",
+  "account_type": "Corriente",
+  "account_number": "string",
+  "start_date": "2022-06-01",
+  "private_role": true,
+  "active": true
+}.to_json
+
+response = resource.post(json_post, content_type: 'application/json')
+#con la conexión lista, parseamos a JSON y terminamos
+json = JSON.parse(response.body)
