a1e1a14b8c07fdeab5c1ef7a639d719ed00f75e2
Pablo <pmella@buk.cl>
2022-06-09 12:09:14 -0400

    ruby: Agregamos un pack de métodos faltantes para el método employees

---
+++ ruby/employees/get-employees-id-subordinates.rb
@@ -0,0 +1,11 @@
+#Método GET de employees/id/subordinates
+#Si la página es cliente.buk.cl, el domain es "cliente"
+domain = 'ACÁ VA EL DOMINIO'
+token = 'ACÁ VA EL TOKEN'
+employee_id = 67
+path = "https://#{domain}.buk.cl/api/v1/chile/employees/#{employee_id}/subordinates"
+#declaramos las variables para invocar el endpoint
+resource = RestClient::Resource.new(path, headers: {auth_token: token})
+response = resource.get(accept: 'application/json')
+#con la conexión lista, parseamos a JSON y terminamos
+json = JSON.parse(response.body)
